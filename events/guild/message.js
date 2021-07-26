b973ce5d075f87b2defbe56ca3617eb1e8534b23
George <Linkeydoo@gmail.com>
2021-07-26 16:56:24 -0400

    Removed more unused stuff

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,15 +1,3 @@
-let text_colors = {
-    server_color: `\x1b[35m`,
-    id_color: `\x1b[31m`,
-    name_color: `\x1b[34m`,
-    channel_color: `\x1b[32m`,
-    textc_color: `\x1b[36m`,
-    voicec_color: `\x1b[32m`,
-    category_color: `\x1b[33m`,
-    bright_hex: `\x1b[1m`,
-    rst_hex: '\x1b[0m'
-};
-let format = require('../../bot_format');
 const bot_settings = require('../../bot-settings');
 
 module.exports = (Discord, client, current_server, message)=>{
